dc4df96437f0d1b9c898e28ad78011b1843b5dae
NaderMohamed325 <nadoorclasher@gmail.com>
2026-09-06 22:29:19 +0300

    add customer service module and enable async processing: include customer-service module in project configuration, enable JPA auditing in AccountServiceApplication, and enable async processing in CustomerServiceApplication

--- customer-service/src/main/java/com/neo/customerservice/CustomerServiceApplication.java
+++ customer-service/src/main/java/com/neo/customerservice/CustomerServiceApplication.java
@@ -3,6 +3,7 @@ package com.neo.customerservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
@@ -12,6 +13,7 @@ import static org.springframework.data.web.config.EnableSpringDataWebSupport.Pag
 @EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 @EnableMethodSecurity
 @EnableScheduling
+@EnableAsync
 public class CustomerServiceApplication {
 
     public static void main(String[] args) {
