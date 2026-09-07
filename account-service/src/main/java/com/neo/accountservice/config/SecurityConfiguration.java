417695747b36c71eb23c155db4838707f4454c2a
NaderMohamed325 <nadoorclasher@gmail.com>
2026-09-07 14:21:25 +0300

    enhance security configuration: disable CSRF protection and improve exception handling setup in SecurityConfiguration

--- account-service/src/main/java/com/neo/accountservice/config/SecurityConfiguration.java
+++ account-service/src/main/java/com/neo/accountservice/config/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.AuthenticationEntryPoint;
@@ -44,7 +45,8 @@ public class SecurityConfiguration {
         log.info("Configuring account-service security filter chain");
 
         http.
-                exceptionHandling(exception -> exception
+                csrf(AbstractHttpConfigurer::disable)
+                .exceptionHandling(exception -> exception
                         .authenticationEntryPoint(authenticationEntryPoint)
                 )
 
